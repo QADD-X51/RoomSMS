b6698782040f76efd63a52e60ac59bd1b168e56b
QADD <tudorps2@yahoo.com>
2023-12-08 21:53:51 +0200

    Added a few dialogs
    Added some dialogs for the chat settings activity.

--- RoomSMSclient/app/src/main/java/com/example/roomsms/activities/ChatSettingsActivity.java
+++ RoomSMSclient/app/src/main/java/com/example/roomsms/activities/ChatSettingsActivity.java
@@ -2,12 +2,16 @@ package com.example.roomsms.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.roomsms.R;
@@ -55,8 +59,100 @@ public class ChatSettingsActivity extends AppCompatActivity {
         settingsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(ChatSettingsActivity.this, "Clicked: " + settingsArray.get(i), Toast.LENGTH_LONG).show();
+                switch(settingsArray.get(i))
+                {
+                    case "Add User To Room":
+                        break;
+                    case "Manage Users":
+                        break;
+                    case "Change Room Name":
+                        break;
+                    case "Leave Room":
+                        showLeaveRoomDialog();
+                        break;
+                    case "Delete Room":
+                        showDeleteRoomDialog();
+                        break;
+                    default:
+                        Toast.makeText(ChatSettingsActivity.this, "What did you click?", Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    private void showLeaveRoomDialog() {
+        Dialog dialog = new Dialog(ChatSettingsActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.dialog_are_you_sure);
+
+        TextView title = findViewById(R.id.DialogTitle);
+        TextView message = findViewById(R.id.DialogMessage);
+        Button noButton = findViewById(R.id.NoButton);
+        Button yesButton = findViewById(R.id.YesButton);
+
+        title.setText(getResources().getString(R.string.dialog_leave_room_title));
+        message.setText(getResources().getString((R.string.dialog_leave_room_message)));
+
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Make it do stuff
+                Toast.makeText(ChatSettingsActivity.this, "Left Room", Toast.LENGTH_LONG).show();
+                dialog.cancel();
+                goBackToMainActivity();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void showDeleteRoomDialog() {
+        Dialog dialog = new Dialog(ChatSettingsActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.dialog_are_you_sure);
+
+        TextView title = dialog.findViewById(R.id.DialogTitle);
+        TextView message = dialog.findViewById(R.id.DialogMessage);
+        Button noButton = dialog.findViewById(R.id.NoButton);
+        Button yesButton = dialog.findViewById(R.id.YesButton);
+
+        title.setText(getResources().getString(R.string.dialog_delete_room_title));
+        message.setText(getResources().getString(R.string.dialog_delete_room_message));
+
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Make it do stuff
+                Toast.makeText(ChatSettingsActivity.this, "Deleted Room", Toast.LENGTH_LONG).show();
+                dialog.cancel();
+                goBackToMainActivity();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void goBackToMainActivity() {
+        Intent intent;
+        intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        finish();
+        startActivity(intent);
     }
 }
